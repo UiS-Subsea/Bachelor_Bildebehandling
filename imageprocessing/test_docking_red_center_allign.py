1dd4e2f741f053264815014ad88975f2fe1382c4
Daniel Edvardsen <danedvar@gmail.com>
2023-01-23 21:48:00 +0100

    Change path to run from root folder

--- imageprocessing/test_docking_red_center_allign.py
+++ imageprocessing/test_docking_red_center_allign.py
@@ -3,15 +3,15 @@ from docking_red_center_allign import docking_red_center_allign
 
 class Test_docking_red_center_allign(unittest.TestCase):
     def test_move_forward(self):
-        dock_command = docking_red_center_allign("images\move_forward_720.png")
+        dock_command = docking_red_center_allign("imageprocessing\images\move_forward_720.png")
         self.assertEqual(dock_command, "MOVE FORWARD")
 
     def test_auto_stop_rov(self):
-        dock_command = docking_red_center_allign("images\dockingstation720_close.png")
+        dock_command = docking_red_center_allign("imageprocessing\images\dockingstation720_close.png")
         self.assertEqual(dock_command, "STOP ROV")
 
     def test_allign_centers(self):
-        dock_command = docking_red_center_allign("images\dockingstation720.png")
+        dock_command = docking_red_center_allign("imageprocessing\images\dockingstation720.png")
         self.assertEqual((-105, -36), dock_command)
 
 

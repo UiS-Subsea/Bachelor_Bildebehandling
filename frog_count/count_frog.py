e22be5978a49313d637e5d687484b6a30d4b3b34
Chipok0101 <c.pokhrel@stud.uis.no>
2023-01-27 13:56:40 +0100

    this works :)

--- frog_count/count_frog.py
+++ frog_count/count_frog.py
@@ -3,17 +3,24 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-image = cv2.imread('froggos.jpg')
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-blur = cv2.GaussianBlur(gray, (10,10), 0)
-canny = cv2.Canny(blur, 30, 120, 3)
-dilated = cv2.dilate(canny, (1, 1), iterations=0)
-
-(cnt, hierarchy) = cv2.findContours(
-	dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+image = cv2.imread('froggos4.jpg')
+test = cv2.cvtColor(image, cv2.COLOR_RGB2HLS_FULL)
+blur = cv2.GaussianBlur(test, (11, 13), 0)
+canny = cv2.Canny(blur, 70, 120, 13)
+dilated = cv2.dilate(canny, (1, 1), iterations=8)
+(cnt, hierarchy) = cv2.findContours(dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-cv2.drawContours(rgb, cnt, -1, (0, 255, 0), 2)
+cv2.drawContours(rgb, cnt, -1, (255, 0, 0), 5)
 
+print(f"There are {len(cnt)} frogs")
 
-print("frogs in the image : ", len(cnt))
+# (cnt, hierarchy) = cv2.findContours(test.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cv2.imshow("Image", test)
+cv2.imshow("Blur", blur)
+cv2.imshow("Canny", canny)
+cv2.imshow("Dilated", dilated)
+cv2.imshow("Contours", rgb)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# print("frogs in the image : ", len(cnt))

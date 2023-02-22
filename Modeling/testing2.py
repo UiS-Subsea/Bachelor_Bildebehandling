4afc225392e35c50f934c164ca7ffdd9ab29eecb
Chipok0101 <c.pokhrel@stud.uis.no>
2023-02-22 12:25:12 +0100

    added images

--- Modeling/testing2.py
+++ Modeling/testing2.py
@@ -1,12 +1,18 @@
-import bpy
 import os
 import matplotlib.pyplot as plt
+import cv2
+from model_functions import *
 
 image_dir = 'Modeling/Images/'
 images = [f for f in os.listdir(image_dir) if f.endswith('.jpg')]
 
 
-img = plt.imread("Modeling/Images/Glass1.jpg")
-print(os.getcwd())
-plt.imshow(img)
-plt.show()
+for image in images:
+    image_path = image_dir + image
+    the_image = cv2.imread(image_path)
+
+    print(the_image.shape)
+    resized_img = cv2.resize(the_image, (720, 960))
+    
+    
+

b7f41abcb0ab7946e3b5ae10693393522c854bb4
Chipok0101 <c.pokhrel@stud.uis.no>
2023-02-27 10:56:02 +0100

    starting 3d model test

---
+++ 3d_testing/distortions.py
@@ -0,0 +1,44 @@
+import numpy as np
+import cv2
+import glob
+
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*6,3), np.float32)
+objp[:,:2] = np.mgrid[0:6,0:6].T.reshape(-1,2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+
+images = glob.glob("3d_testing//*.jpg")
+
+for fname in images:
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    cv2.imshow('img',gray)
+    cv2.waitKey(500)
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray, (6,6),None)
+    
+
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        print("found corners")
+        objpoints.append(objp)
+
+        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+        imgpoints.append(corners)
+
+        # Draw and display the corners
+        cv2.drawChessboardCorners(img, (6,6), corners2, ret)
+        cv2.imshow('img',img)
+        cv2.waitKey(0)
+
+cv2.destroyAllWindows()
+
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+
+np.savez("3d_testing//calibration.npz", mtx=mtx, dist=dist, rvecs=rvecs, tvecs=tvecs)

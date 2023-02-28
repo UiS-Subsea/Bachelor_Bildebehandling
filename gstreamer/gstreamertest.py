9d0e41feb5df3b3b059cf80183f37447e57fabf4
chiranpokhrel <c.pokhrel@stud.uis.no>
2023-02-28 12:03:43 +0100

    new gst feed

--- gstreamer/gstreamertest.py
+++ gstreamer/gstreamertest.py
@@ -1,9 +1,9 @@
 import cv2
 
-gst_feed = "-v udpsrc port=5000 ! application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96 ! rtph264depay ! h264parse ! decodebin ! videoconvert ! appsink sync=false"
+
+gst_feed = "-v udpsrc multicast-group=224.1.1.1 auto-multicast=true multicast-iface=eth0 port=5000 ! application/x-rtp, media=video, clock-rate=90000, encoding-name=H264, payload=96 ! rtph264depay ! h264parse ! decodebin ! videoconvert ! appsink sync=false"
 
 cap = cv2.VideoCapture(gst_feed, cv2.CAP_GSTREAMER)
-print(cv2.getBuildInformation())
 while cap.isOpened():
     while True:
         ret, frame = cap.read()

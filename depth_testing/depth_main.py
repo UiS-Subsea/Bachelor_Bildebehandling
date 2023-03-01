8f7f74ddb64920d71ad22d249bd43b80107c36fc
chiranpokhrel <c.pokhrel@stud.uis.no>
2023-03-01 15:19:22 +0100

    depth func and new imgs

--- depth_testing/depth_main.py
+++ depth_testing/depth_main.py
@@ -5,19 +5,21 @@ import time
 from find_center_red import find_center_of_red
 from depth_find import find_depth
 
-base = 9 #baseline: 10cm between the two cameras [cm]
-focal_length = 6 #camera lense focal lenght, dont know what this means [cm]
-fov = 56.6 #Camera field of view in the horisontal plane [degrees]
 
-img_left = cv2.imread('depth_testing/3dmodel_img1.png')
-red_center_left, radius_left = find_center_of_red(img_left)
 
-img_right = cv2.imread('depth_testing/3dmodel_img2.png')
-red_center_right, radius_right = find_center_of_red(img_right)
+img_left = cv2.imread('depth_testing//3d_model_img1.png')
+img_right  = cv2.imread('depth_testing//3d_model_img2.png')
 
-depth = find_depth(red_center_left, red_center_right, img_left, img_right, base, focal_length, fov)
+def find_depth2(image1, image2):
+    base = 6 #baseline: 10cm between the two cameras [cm]
+    focal_length = 0.26 #camera lense focal lenght, dont know what this means [cm]
+    fov = 73 #Camera field of view in the horisontal plane [degrees]
+    red_center_left, radius_left = find_center_of_red(img_left)
+    red_center_right, radius_right = find_center_of_red(img_right)
+    depth = find_depth(red_center_left, red_center_right, img_left, img_right, base, focal_length, fov)
+    return depth
 
-print(depth)
+print(find_depth2(img_left, img_right))
 
 
 

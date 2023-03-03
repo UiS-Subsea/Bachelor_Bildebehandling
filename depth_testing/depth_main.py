8495dda9a2d87d4a1686dbbea786776bde1832d8
FlipsterSH <260761@uis.no>
2023-03-03 15:45:50 +0100

    tested and completed depth measure technology

--- depth_testing/depth_main.py
+++ depth_testing/depth_main.py
@@ -9,8 +9,9 @@ from depth_find import find_depth
 
 def find_depth_main(image1, image2):
     base = 6 #baseline: 10cm between the two cameras [cm]
-    focal_length = 0.26 #camera lense focal lenght, dont know what this means [cm]
-    fov = 73 #Camera field of view in the horisontal plane [degrees]
+    focal_length = 2.6 #camera lense focal lenght, dont know what this means [mm] 
+    fov = 69 #Camera field of view in the horisontal plane [degrees]
+
     red_center_left, radius_left = find_center_of_red(img_left)
     red_center_right, radius_right = find_center_of_red(img_right)
     depth = find_depth(red_center_left, red_center_right, img_left, img_right, base, focal_length, fov)
@@ -19,8 +20,8 @@ def find_depth_main(image1, image2):
 
 
 if __name__ == "__main__":
-    img_left = cv2.imread('depth_testing/3dmodel_img1.png')
-    img_right  = cv2.imread('depth_testing/3dmodel_img2.png')
+    img_left = cv2.imread('depth_testing/125cm_test1_1080.png')
+    img_right  = cv2.imread('depth_testing/125cm_test2_1080.png')
     print(find_depth_main(img_left, img_right))
 
 
--- depth_testing/find_center_red.py
+++ depth_testing/find_center_red.py
@@ -7,14 +7,20 @@ import time
 def find_center_of_red(img):
     #create a range for isolating only red
     #the values in the range may be tweaked for any color or colorvariation
-    low = (0, 0, 0) #lowest end of the range (black)
-    high = (100, 100, 255) #highest end of the range (red)
+    low = (0, 0, 50) #lowest end of the range (black)
+    high = (30, 30, 100) #highest end of the range (red)
+
+    blurred = cv2.GaussianBlur(img, (11, 13), 0)
 
     #creating a mask using the inRange() function and the low, high range
-    mask1 = cv2.inRange(img, low, high)
+    mask1 = cv2.inRange(blurred, low, high)
+
+    dilated = cv2.dilate(mask1, None, iterations=10)
+
+    blurred2 = cv2.GaussianBlur(dilated, (11, 13), 0)
 
     #using bitwise_and() to convert from mask to actual image format 
-    red_isolated = cv2.bitwise_and(img, img, mask=mask1)
+    red_isolated = cv2.bitwise_and(img, img, mask=blurred2)
 
     #use canny edge detection to find the edges of the red circle
     #input 2 and 3 specify min and max Val for detecting edge, though using different-
@@ -26,6 +32,7 @@ def find_center_of_red(img):
     #cv2.CHAIN_APPROX_SIMPLE means the contour is defined by a simple chain of point and not every single point-
     #to save performance
     (cnt, hierarchy) = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    #cv2.drawContours(img, cnt, -1, (255, 0, 0), 5)
     # cv2.drawContours(red_isolated, cnt, -1, (255, 0, 0), 7)
 
     #there may be a lot of noise in the image, to find the correct contoure, we loop through the list of contoures (cnt)
@@ -38,8 +45,11 @@ def find_center_of_red(img):
     center = (int(red_center[0][0]), int(red_center[0][1]))
     radius = int(red_center[1])
     cv2.circle(img, center, radius, (0, 255, 0), 2)
+    print(center, radius)
 
+    cv2.imshow("blurred2", blurred2)
     cv2.imshow("img", img)
+    cv2.imshow("canny", canny)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 

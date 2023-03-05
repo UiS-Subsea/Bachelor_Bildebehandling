48b8bdc4533ddc348852658f393b48451ee77db7
chiranpokhrel <c.pokhrel@stud.uis.no>
2023-03-05 09:50:40 +0100

    Camerafeed now works async, press S to save current frame, press Q to quit

--- camerafeed/async_tests.py
+++ camerafeed/async_tests.py
@@ -5,27 +5,21 @@ class CamerafeedAsync:
     def __init__(self) -> None:
         self.cap = cv2.VideoCapture(0)
         self.started = False
-        self.grabbed, self.frame = self.cap.read()
+        self.frame, self.grabbed = self.cap.read()
     
-    # Task to update the frame
     async def update(self):
         while self.started:
             self.grabbed, self.frame = self.cap.read()
             await asyncio.sleep(0)
             
-    # Task to show the frame
     async def show_frame(self):
         while self.started:
             cv2.imshow("frame", self.frame)
             await asyncio.sleep(0)
-    
-    # Returns frame if needed (Not used)
+            
     def read(self):
         return self.grabbed, self.frame
     
-    # Task to handle key commands
-    # press 'q' to quit
-    # press 's' to save the frame
     async def key_commands(self):
         while self.started:
             key = cv2.waitKey(1)
@@ -37,26 +31,23 @@ class CamerafeedAsync:
                 break
             await asyncio.sleep(0)
         
-    # Start running the camerafeed and collects tasks
     async def start(self):
         self.started = True
         # tasks = [self.update(), self.show_frame(), self.key_commands()]
         # await asyncio.gather(*tasks)
         await self.do_tasks(self.update(), self.show_frame(), self.key_commands())
         
-    # Run the tasks asynchronously
     async def do_tasks(self, *tasks):
         the_tasks = []
         for task in tasks:
             the_tasks.append(task)
         await asyncio.gather(*the_tasks)
               
-    # Destructor, Stops the camerafeed
     def __del__(self):
         self.cap.release()
         cv2.destroyAllWindows()
         
-# Main function to run the camerafeed
+        
 async def main():
     cam = CamerafeedAsync()
     await cam.start()

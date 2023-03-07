e4b0c4292df4264ff1f44e1d1b55489934853130
FlipsterSH <260761@uis.no>
2023-03-07 16:26:36 +0100

    finds area of circle around conture using depth

--- depth_testing/depth_main.py
+++ depth_testing/depth_main.py
@@ -10,23 +10,38 @@ from depth_find import find_depth
 def find_depth_main(img_left, img_right):
     base = 6 #baseline: 10cm between the two cameras [cm]
     focal_length = 2.6 #camera lense focal lenght, dont know what this means [mm] 
-    fov = 69 #Camera field of view in the horisontal plane [degrees]
+    fov = 70 #Camera field of view in the horisontal plane [degrees]
 
     # red_center_left, radius_left = find_center_of_red(img_left, low_range=(47, 37, 65), high_range=(55, 47, 75))
     # red_center_right, radius_right = find_center_of_red(img_right, low_range=(60, 50, 80), high_range=(80, 60, 100))
     red_center_left, radius_left = find_center_of_red(img_left)
     red_center_right, radius_right = find_center_of_red(img_right)
+    radius = (radius_left + radius_right) / 2 #avg value of the radiuses
     depth = find_depth(red_center_left, red_center_right, img_left, img_right, base, focal_length, fov)
-    return depth
+    return depth, radius
 
+
+def find_aread_of_circle(depth, radius):
+    fov = 69
+    frame_px_width = 1920 #CANNOT BE HARDCODED!!!
+    view_width_cm = 2 * (math.tan(math.radians(fov / 2)) * depth)
+    radius_width_ratio = radius / frame_px_width
+    radius_in_cm = view_width_cm * radius_width_ratio
+    area = math.pi * (radius_in_cm ** 2)
+
+    return area
 
 
 
 
 if __name__ == "__main__":
-    img_left = cv2.imread('depth_testing/93cm_test1_1080.png')
-    img_right  = cv2.imread('depth_testing/93cm_test2_1080.png')
-    print(find_depth_main(img_left, img_right))
+    img_left = cv2.imread('depth_testing/125cm_test1_1080.png')
+    img_right  = cv2.imread('depth_testing/125cm_test2_1080.png')
+    depth, radius = find_depth_main(img_left, img_right)
+    area = find_aread_of_circle(depth, radius)
+    print(depth, radius)
+    print(area)
+    
 
 
 

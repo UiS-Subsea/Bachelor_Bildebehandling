70d3d45c147c6429790130b3365f31a899e66974
Filip <filippo1339@gmail.com>
2023-05-15 10:20:24 +0200

    added frogcount into videocount

--- Imageprocessing/Main_Classes/autonomous_transect_main.py
+++ Imageprocessing/Main_Classes/autonomous_transect_main.py
@@ -149,8 +149,184 @@ class AutonomousTransect:
         else:
             # print("Waiting for ROV to stabilize angle")
             return
-         
+        
 
+
+class Frog:
+    def __init__(self, rectangle):
+        self.detectionCounter = 1
+        self.rectangle = rectangle
+        
+
+class FrogCount:
+    def __init__(self):
+        self.currFrogs = [] # List of currently detected Frog objects
+        self.prevFrogs = [] # List of detected Frog objects in previous frame
+        self.frog_counter = 0
+        self.detection_counter_threshold = 5
+        self.frame = None
+        self.fgbg = cv2.createBackgroundSubtractorKNN()
+        self.kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+        
+        self.check_previous_algo = "ADVANCED"
+
+    # Starts the frog counting process
+    def update(self, image, drawImage = False):
+        self.frame = image
+        
+        # Calling the frog detection functions
+        rectanglesNoRed = self.frogDetectionNoRed(image)
+        rectanglesNoGrout = self.frogDetectionNoGrout(image)
+        allRectangles = rectanglesNoRed + rectanglesNoGrout
+        filteredRectangles = self.rectangleOverlapFilter(allRectangles)
+        
+        if drawImage:
+            for rect in filteredRectangles:
+                x,y,w,h = rect
+                cv2.rectangle(image, (x,y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.imshow("Image", image)
+
+        # Converts the rectangles into Frog objects, used for Advanced frog counting algorithm
+        frogRectangles = []
+        for rectangle in filteredRectangles:
+            frogRectangles.append(Frog(rectangle))
+        
+        self.currFrogs = frogRectangles
+        
+        if self.check_previous_algo == "BASIC":
+            self.checkPreviousFrogsBasic()
+            
+        if self.check_previous_algo == "ADVANCED":
+            self.checkPreviousFrogsAdvanced()
+
+        return self.frog_counter
+    
+    # Basic algorithm for checking if a frog has been detected in last frame. Only works if detection is extremely accurate, ie. no noise and frogs always detected. Not used in final version
+    def checkPreviousFrogsBasic(self): 
+        numFrogsDetected = len(self.currFrogs)
+        allFrogs = self.prevFrogs + self.currFrogs
+        numAllFrogs = len(allFrogs)
+        FilteredFrogs = self.rectangleOverlapFilter(allFrogs)
+        numRemovedFrogs = numAllFrogs - len(FilteredFrogs)
+        self.frog_counter += numFrogsDetected - numRemovedFrogs
+        self.prevFrogs = self.currFrogs
+
+    # Advanced algorithm for checking if a frog has been detected in last frame. Less susceptible to noise and missed detections
+    def checkPreviousFrogsAdvanced(self):
+        allFrogs = self.currFrogs + self.prevFrogs
+        theRectangles = []
+        for frog in allFrogs:
+            theRectangles.append(frog.rectangle)
+        OverlapPairs = self.rectangleOverlapFilterNoRemove(theRectangles.copy())
+        for OverlapPair in OverlapPairs:
+            # OverlapPair = touple of two indexes, of rectangles that overlap
+            if allFrogs[OverlapPair[0]].detectionCounter <= allFrogs[OverlapPair[1]].detectionCounter:
+                
+                allFrogs[OverlapPair[0]].detectionCounter = allFrogs[OverlapPair[1]].detectionCounter + 1
+        
+        self.prevFrogs = self.currFrogs
+        for frog in self.prevFrogs:
+            if frog.detectionCounter == self.detection_counter_threshold:
+                self.frog_counter += 1
+        self.currFrogs = []
+        
+    def frogDetectionNoRed(self, image): # Does not detect red frogs
+        # Threshold for red color of the image. Goal is for thresh1 to detect the grouts, and thresh2 to detect the grouts and frogs
+        # the difference would then be only the frogs
+        thresh1 = cv2.threshold(image[:,:,0], 70, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C)[1]
+        thresh2 = cv2.threshold(image[:,:,0], 200, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C)[1]
+        difference = cv2.subtract(thresh2, thresh1)
+        
+        # Blur to remove noise
+        blur_difference = cv2.GaussianBlur(difference, (41, 41), 0)
+        new_thresh = cv2.threshold(blur_difference, 0, 255, cv2.THRESH_OTSU)[1]
+        
+        contours, _ = cv2.findContours(new_thresh.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        frogRectangles = self.contourFiltration(contours)
+        return frogRectangles
+    
+    def frogDetectionNoGrout(self, image): # Does not detect frogs in dark tile grouts or frogs which match the pool floor color
+        # Same process as frogDetectionNoRed, but with different thresholds and now using grayscale image instead of red channel
+        hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS_FULL)
+        gray = cv2.cvtColor(hls, cv2.COLOR_RGB2GRAY)
+        thresh1 = cv2.threshold(gray, 90, 255, cv2.THRESH_OTSU)[1]
+        thresh2 = cv2.threshold(gray, 120, 255, cv2.THRESH_TRIANGLE)[1]
+        difference = cv2.subtract(thresh1, thresh2)
+        diff_blur = cv2.GaussianBlur(difference, (71, 71), 0)
+        dilate_blur = cv2.dilate(diff_blur, None, iterations=6)
+        newThreshold = cv2.threshold(dilate_blur, 0, 255, cv2.THRESH_OTSU)[1]
+        
+        contours, _ = cv2.findContours(newThreshold.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        frogRectangles = self.contourFiltration(contours)
+        return frogRectangles
+
+    
+    def contourFiltration(self, contours, epsilonValue = 0.03, noise_threshhold_lower = 40, noise_threshhold_upper = 300): # Finds frogs using various filters
+        frog_rectangles = []
+        for contour in contours:
+            # epsilon value can be tweaked, higher value allows for larger approximated polygon, more likely to have less sides
+            epsilon = epsilonValue * cv2.arcLength(contour, True)
+            # approx is the polygonal approximation of the contour
+            approx = cv2.approxPolyDP(contour, epsilon, True)
+            # rect = cv2.minAreaRect(contour)
+            # box = cv2.boxPoints(rect) # Rectangle, not rotated
+            # box = np.int0(box)
+            if 10 > len(approx) > 4: # More than 4 sides means its more round than a square, more sides means more circular
+                # w,h width and height
+                x, y, w, h = cv2.boundingRect(approx) # Rectangle, rotated
+                if 0.7 < w/h < 1.3: # If the width and height are within 20% of each other, it is a square
+                    # Noise threshhold to ignore small and large contours, can be tweaked
+                    if  w > noise_threshhold_lower < h and w < noise_threshhold_upper > h:
+                        frog_rectangles.append((x,y,w,h))
+                        cv2.rectangle(self.frame, (x, y), (x+w, y+h), (255, 0, 0), 3)
+        return frog_rectangles
+    
+    def rectangleOverlapFilter(self, rectangles): # Filters out rectangles that overlap O(n^2)
+        if len(rectangles) >= 2:
+            n = 0
+            for rectangle in rectangles:
+                n += 1
+                for index in range(n, len(rectangles)):
+                    x,y,w,h = rectangle
+                    x2,y2,w2,h2 = rectangles[index]
+                    
+                    # Checks if rectangles overlap in x and y axis
+                    # From https://www.geeksforgeeks.org/find-two-rectangles-overlap/
+                    if x + w < x2 or x2 + w2 < x:
+                        continue
+                    
+                    elif y + h < y2 or y2 + h2 < y:
+                        continue
+                    
+                    else:
+                        rectangles.pop(index)
+                        break
+                    
+        return rectangles
+    
+    def rectangleOverlapFilterNoRemove(self, rectangles): # Finds rectangles that overlap, but does not remove them. Returns a list of pairs of overlapping rectangles
+        overlappedRectsPairs = []
+        if len(rectangles) >= 2:
+            n = 0
+            for rectangle in rectangles:
+                n += 1
+                for index in range(n, len(rectangles)):
+                    x,y,w,h = rectangle
+                    x2,y2,w2,h2 = rectangles[index]
+                    
+                    # Checks if rectangles overlap in x and y axis
+                    # From https://www.geeksforgeeks.org/find-two-rectangles-overlap/
+                    if x + w < x2 or x2 + w2 < x:
+                        continue
+                    
+                    elif y + h < y2 or y2 + h2 < y:
+                        continue
+                    
+                    else:
+                        overlappedRectsPairs.append((n, index))
+                        break
+                    
+        return overlappedRectsPairs
 
 
     
